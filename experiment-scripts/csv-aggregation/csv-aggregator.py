3ae95c0d5491133fd98a7f7af6083eed82d0f3c9
David Martínez García <94689492+david-martinez-garcia@users.noreply.github.com>
2025-07-11 12:05:09 +0200

    Upload first version of experiment data CSV aggregator

---
+++ experiment-scripts/csv-aggregation/csv-aggregator.py
@@ -0,0 +1,150 @@
+__name__ = "B5G-ACROSS-TC32 -- Experiment data to CSV aggregator"
+__version__ = "0.0.1"
+__author__ = "David Martínez García <https://github.com/david-martinez-garcia>"
+__credits__ = [
+    "GIROS DIT-UPM <https://github.com/giros-dit>",
+    "Luis Bellido Triana <https://github.com/lbellido>",
+    "Pablo Fernández López <https://github.com/pablofl01>",
+    "Mario Vicente Albertos <https://github.com/mariov22>",
+    "Alejandro Villaseca Martínez <https://github.com/avillaseca01>",
+    "Daniel González Sánchez <https://github.com/daniel-gonzalez-sanchez>",
+    "Carlos Mariano Lentisco Sánchez <https://github.com/clentisco>"
+]
+
+## -- BEGIN IMPORT STATEMENTS -- ##
+
+import boto3
+import csv
+import json
+import logging
+import os
+
+## -- END IMPORT STATEMENTS -- ##
+
+## -- BEGIN LOGGING CONFIGURATION -- ##
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format = '%(asctime)s %(levelname)-8s %(message)s',
+    level = logging.DEBUG,
+    datefmt = '%d-%m-%Y %H:%M:%S'
+)
+
+## -- END LOGGING CONFIGURATION -- ##
+
+## -- BEGIN CONSTANTS DEFINITION -- ##
+
+### --- S3 STORAGE CONFIGURATION --- ###
+
+# S3_ENDPOINT = os.getenv("S3_ENDPOINT")
+S3_ENDPOINT = "http://138.4.21.162:9000"
+# S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+S3_ACCESS_KEY = "YLhDyu4HyTH7h7JEYZBi"
+# S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_SECRET_KEY = "BDrr7X8x4OgzwM2rJeW0k6oeV50JcLOYRcVEoc2z"
+
+# Experiment data are saved in independent buckets.
+# The name of the bucket will be the name/ID of the experiment.
+# S3_BUCKET = os.getenv("S3_BUCKET")
+S3_BUCKET = "experiment-csv-agg-test"
+
+### --- --- ###
+
+### --- CSV HEADERS --- ###
+
+CSV_HEADERS = ["experiment_id", "router_id", "epoch_timestamp", "power_consumption_watts"]
+
+### --- --- ###
+
+## -- END CONSTANTS DEFINITION -- ##
+
+## -- BEGIN MAIN CODE -- ##
+
+logger.info("Script invocated.")
+
+logger.info("---")
+
+# Initialize S3 client:
+logger.info("Initializing S3 client...")
+
+s3_client = boto3.client(
+    "s3",
+    endpoint_url = S3_ENDPOINT,
+    aws_access_key_id = S3_ACCESS_KEY,
+    aws_secret_access_key = S3_SECRET_KEY,
+    region_name = "local" 
+)
+
+logger.info("Done.")
+
+logger.info("---")
+
+# Create output CSV file and write headers line:
+logger.info("Creating output CSV file and writer object...")
+
+csv_output_file = open(file = S3_BUCKET + ".csv", mode = "w", newline = "")
+csv_writer = csv.writer(csv_output_file)
+
+logger.info("Done.")
+
+logger.info("---")
+
+logger.info("Writing headers to CSV file...")
+csv_writer.writerow(CSV_HEADERS)
+logger.info("Done.")
+
+logger.info("---")
+
+# Get JSON files from S3 storage. These files contain the desired router metrics that are to be aggregated
+# in the output CSV file.
+# Help: https://stackoverflow.com/questions/36205481/read-file-content-from-s3-bucket-with-boto3
+try:
+    logger.info("Trying to retrieve JSON files from S3 storage...")
+    response = s3_client.list_objects_v2(Bucket = S3_BUCKET, Prefix = "")
+    logger.info("Done.")
+
+    logger.info("---")
+
+    for object in response.get("Contents"):
+        logger.info("Trying to retrieve data from JSON file...")
+        data = s3_client.get_object(Bucket = S3_BUCKET, Key = object.get('Key'))
+        metrics_content = json.loads(data['Body'].read().decode("utf-8").strip())
+        logger.info("Done.")
+
+        logger.info("---")
+
+        # For every retrieved JSON file, parse it to get metrics and write them to output CSV file.
+        logger.info("Trying to parse JSON data and retrieve desired metrics...")
+        experiment_id = metrics_content["experiment_id"]
+        router_id = metrics_content["node_exporter"].split(":")[0]
+        epoch_timestamp = metrics_content["epoch_timestamp"]
+        for output_ml_metric in metrics_content["output_ml_metrics"]:
+            if output_ml_metric["name"] == "node_network_power_consumption":
+                power_consumption_watts = output_ml_metric["value"]
+        csv_metrics = [
+            experiment_id,
+            router_id,
+            epoch_timestamp,
+            power_consumption_watts
+        ]
+        logger.info("Done.")
+
+        logger.info("---")
+
+        logger.info("Trying to write metrics to output CSV file...")
+        csv_writer.writerow(csv_metrics)
+        logger.info("Done.")
+
+        logger.info("---")
+    # Finally, the output CSV file is closed:
+    logger.info("Closing output CSV file...")
+    csv_output_file.close()
+    logger.info("Done.")
+    
+    logger.info("---")
+except Exception as e:
+    logger.exception(f"Exception while retrieving data from S3 storage: {e}")
+
+logger.info("All done.")
+
+## -- END MAIN CODE -- ##
