c3f85d22dffae4e360388a9a2b0842ec53197230
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2025-11-23 18:39:25 +0100

    Cleaning CSV files and including sample file to define enviroment variables needed to run the csv-aggregator.py and csv-aggregator-with-flows-definition-file-time-limit.py Python scripts.

---
+++ experiment-scripts/csv-aggregation/env.sh
@@ -0,0 +1,5 @@
+export S3_ENDPOINT=<MinIO_server_endpoint>
+export S3_ACCESS_KEY=<MinIO_access_key>
+export S3_SECRET_KEY=<MinIO_secret_key>
+export S3_BUCKET=energy-aware-1
+export S3_FLOWS_FILE_DATETIME_PREFIX=<flows_file_datetime_prefix> # e.g., flows_20251106
